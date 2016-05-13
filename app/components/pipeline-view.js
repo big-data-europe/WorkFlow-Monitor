bdbc2d07615a3e1a8c502e0aa9cf2740a72d0fe6
Erika Pauwels <erika.pauwels@gmail.com>
2016-05-13 16:42:29 +0200

    Remove unused component

--- app/components/pipeline-view.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  title: Ember.computed('title', function() {
-    return !!this.get('pipeline.title') ? this.get('pipeline.title') : 'No title';
-  })
-});
